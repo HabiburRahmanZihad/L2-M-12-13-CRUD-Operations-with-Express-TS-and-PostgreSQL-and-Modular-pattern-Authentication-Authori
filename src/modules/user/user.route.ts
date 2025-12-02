9feaead85f884c921113c9b7557d779bdb90ed3a
Habibur Rahman Zihad <e241024@ugrad.iiuc.ac.bd>
2025-12-02 16:04:05 +0600

    try to make unique folder for better understand

--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -1,136 +1,21 @@
-import { Request, Response, Router } from "express";
-import { pool } from "../../config/db";
+import { Router } from "express";
+import { userController } from "./user.controller";
 
 const router = Router();
 
 // Create a new user
-router.post("/", async (req: Request, res: Response) => {
-  const { name, email } = req.body;
-
-  try {
-    const result = await pool.query(
-      'INSERT INTO users (name, email) VALUES ($1, $2) RETURNING *',
-      [name, email]
-    );
-    console.log(result.rows[0]);
-    res.status(201).json({
-      success: true,
-      message: 'User created successfully',
-      user: result.rows[0]
-    });
-  } catch (error: any) {
-    res.status(500).json({
-      success: false,
-      message: 'Error inserting data into the database',
-      error: error.message
-    });
-  }
-
-  // console.log(req.body);
-})
+router.post("/", userController.CreateUser);
 
 // Get all users
-router.get('/', async (req: Request, res: Response) => {
-  try {
-    const result = await pool.query('SELECT * FROM users');
-    res.status(200).json({
-      success: true,
-      users: result.rows
-    });
-  } catch (error: any) {
-    res.status(500).json({
-      success: false,
-      message: 'Error fetching users from the database',
-      error: error.message
-    });
-  }
-});
-
+router.get('/', userController.GetAllUsers);
 
 // Get a single user by ID
-router.get('/:id', async (req: Request, res: Response) => {
-  const userId = req.params.id;
-  try {
-    const result = await pool.query('SELECT * FROM users WHERE id = $1', [userId]);
-    if (result.rows.length === 0) {
-      return res.status(404).json({
-        success: false,
-        message: 'User not found'
-      });
-    }
-    res.status(200).json({
-      success: true,
-      user: result.rows[0]
-    });
-  } catch (error: any) {
-    res.status(500).json({
-      success: false,
-      message: 'Error fetching user from the database',
-      error: error.message
-    });
-  }
-});
-
+router.get('/:id', userController.GetUserById);
 
 //Update a user by ID
-router.put('/:id', async (req: Request, res: Response) => {
-  const userId = req.params.id;
-  const { name, email } = req.body;
-  try {
-    const result = await pool.query(
-      `UPDATE users
-        SET name = $1, email = $2, updated_at = NOW()
-        WHERE id = $3
-        RETURNING *`,
-      [name, email, userId]
-    );
-    if (result.rows.length === 0) {
-      return res.status(404).json({
-        success: false,
-        message: 'User not found'
-      });
-    }
-    res.status(200).json({
-      success: true,
-      message: 'User updated successfully',
-      user: result.rows[0]
-    });
-  } catch (error: any) {
-    res.status(500).json({
-      success: false,
-      message: 'Error updating user in the database',
-      error: error.message
-    });
-  }
-})
-
+router.put('/:id', userController.UpdateUserById);
 
 // Delete a user by ID
-router.delete('/:id', async (req: Request, res: Response) => {
-  const userId = req.params.id;
-  try {
-    const result = await pool.query(
-      'DELETE FROM users WHERE id = $1 RETURNING *',
-      [userId]
-    );
-    if (result.rows.length === 0) {
-      return res.status(404).json({
-        success: false,
-        message: 'User not found'
-      });
-    }
-    res.status(200).json({
-      success: true,
-      message: 'User deleted successfully',
-      user: result.rows[0]
-    });
-  } catch (error: any) {
-    res.status(500).json({
-      success: false,
-      message: 'Error deleting user from the database',
-      error: error.message
-    });
-  }
-})
+router.delete('/:id', userController.DeleteUserById);
 
 export const userRoutes = router;
---
+++ src/modules/user/user.service.ts
@@ -0,0 +1,51 @@
+import { pool } from "../../config/db";
+
+// Create a new user
+const CreateUser = async (name: string, email: string) => {
+  const result = await pool.query(
+    'INSERT INTO users (name, email) VALUES ($1, $2) RETURNING *',
+    [name, email]
+  );
+  return result;
+}
+
+// Get all users
+const GetAllUsers = async () => {
+  const result = await pool.query('SELECT * FROM users');
+  return result;
+}
+
+// Get a single user by ID
+const GetUserById = async (userId: any) => {
+  const result = await pool.query('SELECT * FROM users WHERE id = $1', [userId]);
+  return result;
+}
+
+//Update a user by ID
+const UpdateUserById = async (userId: any, name: string, email: string) => {
+  const result = await pool.query(
+      `UPDATE users
+        SET name = $1, email = $2, updated_at = NOW()
+        WHERE id = $3
+        RETURNING *`,
+      [name, email, userId]
+    );
+  return result;
+}
+
+// Delete a user by ID
+const DeleteUserById = async (userId: any) => {
+  const result = await pool.query(
+    'DELETE FROM users WHERE id = $1 RETURNING *',
+    [userId]
+  );
+  return result;
+}
+
+export const userService = {
+  CreateUser,
+  GetAllUsers,
+  GetUserById,
+  UpdateUserById,
+  DeleteUserById
+};
