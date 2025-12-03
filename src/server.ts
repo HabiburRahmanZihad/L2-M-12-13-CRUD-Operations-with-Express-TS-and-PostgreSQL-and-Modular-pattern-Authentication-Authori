4ba9f5946e765204cf2d8d9e58e346230e40a77c
Habibur Rahman Zihad <e241024@ugrad.iiuc.ac.bd>
2025-12-03 08:27:46 +0600

    making more organized file

--- src/server.ts
+++ src/server.ts
@@ -1,45 +1,7 @@
-import express, { Request, Response } from 'express'
+import app from "./app"
+import config from "./config"
 
-import config from './config';
-import initDb from './config/db';
-import logger from './middleware/logger';
-import { userRoutes } from './modules/user/user.route';
-import { todoRoutes } from './modules/todo/todo.route';
-import { authRoutes } from './modules/auth/auth.routes';
-
-const app = express()
 const port = config.port || 8080;
-
-
-// Middleware to parse JSON bodies
-app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-
-// Initialize the database and create tables if they don't exist
-initDb();
-
-// Root Endpoint
-app.get('/', logger, (req: Request, res: Response) => {
-  res.send('Hello Next Level Programmers!')
-})
-
-
-//User CRUD Operations
-app.use('/users', userRoutes);
-
-// Todo CRUD Operations
-app.use('/todos', todoRoutes);
-
-// Authentication Routes
-app.use('/auth', authRoutes);
-
-//Handle Invalid Routes
-app.use((req: Request, res: Response) => {
-  res.status(404).json({
-    success: false,
-    message: 'Route not found'
-  });
-});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
